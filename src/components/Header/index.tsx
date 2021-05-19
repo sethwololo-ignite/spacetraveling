f444cfa491b88f1d2df91961f21dfcd9ba363ba4
Matheus Henrique <sethwololo@gmail.com>
2021-05-19 14:49:58 -0300

    Listagem dos posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.HeadingContainer}>
+      <div className={styles.HeadingContent}>
+        <Link href="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +26,55 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  console.log(postsPagination);
+  return (
+    <>
+      <Head>
+        <title>Posts | spacetravelling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={styles.container}>
+        <header>
+          <img src="/images/logo.svg" alt="logo" />
+        </header>
 
-//   // TODO
-// };
+        {postsPagination.results.map(post => (
+          <a href="/" className={styles.post} key={post.uid}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <div>
+                <FiCalendar size={20} />
+                <time>{post.first_publication_date}</time>
+              </div>
+              <div>
+                <FiUser size={20} />
+                <span>{post.data.author}</span>
+              </div>
+            </div>
+          </a>
+        ))}
+
+        <button type="button">Carregar mais posts</button>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts'),
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 4,
+    }
+  );
+
+  return {
+    props: {
+      postsPagination: postsResponse,
+    },
+  };
+};
